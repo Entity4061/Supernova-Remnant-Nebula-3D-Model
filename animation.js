0552c74a6237503a2b633e0adfdb4f3bb42814ee
Entity4061 <134186154+Entity4061@users.noreply.github.com>
2024-10-04 18:45:04 +1000

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,6 +1,7 @@
 // Animation loop
 let elapsedTime = 0;
 const duration = 5; // Duration of the animation in seconds
+let animationPhase = 0;
 
 function animate() {
     requestAnimationFrame(animate);
@@ -8,10 +9,18 @@ function animate() {
     elapsedTime += 0.01;
     const t = Math.min(elapsedTime / duration, 1);
 
-    for (let i = 0; i < particleCount; i++) {
-        positions[i * 3] = initialPositions[i * 3] * (1 - t) + targetPositions[i * 3] * t;
-        positions[i * 3 + 1] = initialPositions[i * 3 + 1] * (1 - t) + targetPositions[i * 3 + 1] * t;
-        positions[i * 3 + 2] = initialPositions[i * 3 + 2] * (1 - t) + targetPositions[i * 3 + 2] * t;
+    if (animationPhase === 1) {
+        for (let i = 0; i < particleCount; i++) {
+            positions[i * 3] = initialPositions[i * 3] * (1 - t) + targetPositions[i * 3] * t;
+            positions[i * 3 + 1] = initialPositions[i * 3 + 1] * (1 - t) + targetPositions[i * 3 + 1] * t;
+            positions[i * 3 + 2] = initialPositions[i * 3 + 2] * (1 - t) + targetPositions[i * 3 + 2] * t;
+        }
+    } else if (animationPhase === 2) {
+        for (let i = 0; i < particleCount; i++) {
+            positions[i * 3] = targetPositions[i * 3] * (1 - t) + finalPositions[i * 3] * t;
+            positions[i * 3 + 1] = targetPositions[i * 3 + 1] * (1 - t) + finalPositions[i * 3 + 1] * t;
+            positions[i * 3 + 2] = targetPositions[i * 3 + 2] * (1 - t) + finalPositions[i * 3 + 2] * t;
+        }
     }
 
     particles.attributes.position.needsUpdate = true;
@@ -29,17 +38,26 @@ animate();
 const moveSpeed = 2;
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
-        case 'ArrowUp':
+        case 'w':
             camera.position.z -= moveSpeed;
             break;
-        case 'ArrowDown':
+        case 's':
             camera.position.z += moveSpeed;
             break;
-        case 'ArrowLeft':
+        case 'a':
             camera.position.x -= moveSpeed;
             break;
-        case 'ArrowRight':
+        case 'd':
             camera.position.x += moveSpeed;
+            break;
+        case 'ArrowRight':
+            if (animationPhase === 0) {
+                animationPhase = 1;
+                elapsedTime = 0;
+            } else if (animationPhase === 1) {
+                animationPhase = 2;
+                elapsedTime = 0;
+            }
             break;
     }
 });
